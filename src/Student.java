8fbcfe3d5898ecf42773a29f3e929cfea18e8c23
sepipi01 <sepipi01@wsc.edu>
2025-02-26 11:41:55 -0600

    Added the variables and default constructors.

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,56 @@
 /**
  * A Student in a Course and student's seat
+ *
  * @author Seth Pipim and Isaiah
- * @since 2025.02.21
  * @version 1.0 beta
  * @see <a href="https://github.com/isguil02/Attendance.git">Github Repository</a>
+ * @since 2025.02.21
  */
 
 public class Student {
+    /**
+     * The Students's seat.
+     */
     private int seat;
+
+    /**
+     * The Student's name.
+     */
     private String name;
+
+    /**
+     * The number of on time arrivals the student has had.
+     */
+    private int onTime;
+
+    /**
+     * The number of late arrivals the student has had.
+     */
+    private int late;
+
+    /**
+     * The number of excused absences the student has had.
+     */
+    private int excused;
+
+    /**
+     * The number of unexcused absences the student has had.
+     */
+    private int unexcused;
+
+    /**
+     * Set the Student's fields to default values null and zeroes.
+     * The default constructor is PRIVATE, so it is ONLY called by the overload constructor.
+     */
+
+    private Student() {
+        name = null;
+        seat = 0;
+        onTime = 0;
+        late = 0;
+        excused = 0;
+        unexcused = 0;
+    }// end of default constructor
 
     public Student(int seat, String name) {
         this.seat = seat;

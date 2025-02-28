5460c35f951d68d2bee345410f2895dbe3a318f4
sepipi01 <sepipi01@wsc.edu>
2025-02-28 11:15:59 -0600

    added default constructor and display header

--- src/AttendanceApp.java
+++ src/AttendanceApp.java
@@ -1,2 +1,49 @@
+/**
+ * The Attendance Application
+ *
+ * @author Seth Pipim and Isaiah
+ * @version 1.0 beta
+ * @since 2025.02.21
+ * @see <a href="https://github.com/isguil02/Attendance.git">Github Repository</a>
+ */
 public class AttendanceApp {
+
+    /** The first course section. */
+    private Course section1;
+
+    /** The second course section. */
+    private Course section2;
+
+    /** Standard double dash line for display output */
+    private static String DOUBLE_DASH_LINE = String.format("%50s", "").replace(' ', '=');
+
+    /** Standard single dash line for display output */
+    private static String SINGLE_DASH_LINE = DOUBLE_DASH_LINE.replace('=', '-');
+
+    /** Default constructor that instantiate both course sections. */
+    public AttendanceApp() {
+         section1 = new Course();
+         section2 = new Course();
+
+    }// end of default constructor
+/**
+ *  Displays the Attendance App header. Example:<br>
+ *  <pre>
+ *  =========================================
+ *  Welcome to the Attendance App
+ *  =========================================
+ *  </pre>
+ */
+private void displayAppHeading() {
+    System.out.println("DOUBLE_DASH_LINE");
+    System.out.println("Welcome to Attendance App");
+    System.out.println("DOUBLE_DASH_LINE");
+    System.out.println();
+} // ending of displayAppHeading
+
+private void setupCourses() throws Exception {
+}
+
+public void main() {
+}
 }

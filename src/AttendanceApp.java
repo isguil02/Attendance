2f525a8693ccbcf10782924468a2733d7be0d64d
isguil02 <isguill06@gmail.com>
2025-03-06 23:04:47 -0600

    Ready to test once course is finished

--- src/AttendanceApp.java
+++ src/AttendanceApp.java
@@ -129,20 +129,122 @@ private void setupCourses() throws Exception {
                     break;
                 case 1:
                 case 2:
-                    if (userInput)
-                        updateTeamStats(section1);
+                    if (userInput==1)
+                        updateCourseAttendance(section1);
                     else
-                        updateTeamStats(section2);
+                        updateCourseAttendance(section2);
 
                 case 3:
-                    displayStudentAttendance();
+                    displayAttendanceStatus();
                     break;
                 default:
                     System.out.println("Invalid menu choice = " + userInput);
             } // end of switch
         }
     } // end of mainMenu
-public void main() {
+/**
+ * Update the selected course's attendance.<br>
+ * Calls Input.getIntRange using range from 1 and 55.<br>
+ * Example: <br>
+ * <pre>
+ * Enter Alice's seat number: user input
+ * </pre>
+ * @param course The course to update attendance for.
+ * @throws Exception getStudent could throw an invalid seat number error
+ */
+private void updateCourseAttendance(Course course) throws Exception {
+    int seat;
+    Student student;
+    while (true) {
+        seat = Input.getIntRange("Enter " + course.getName() + "'s seat number: ", 1, 55);
+        student = course.getStudent(seat);
+        if (student == null) {
+            System.out.println("Invalid seat number, please try again!");
+            continue;
+        }
+        updateStudentAttendance(student);
+        break;
+    }
+    System.out.println();
+    System.out.println(SINGLE_DASH_LINE);
+} // end of updateCourseAttendance
+
+/**
+ * Displays the student's name along with the attendance menu. Example:
+ * <pre>
+ * -----------------------------------------
+ * Enter #10 Alice Attendance
+ * -----------------------------------------
+ * 0 = Absent
+ * 1 = Present
+ * -----------------------------------------
+ * Enter Attendance Type: 1
+ * -----------------------------------------
+ * #10 Alice Attendance=Present
+ * -----------------------------------------
+ * </pre>
+ * @param student The student to enter attendance for
+ */
+private void updateStudentAttendance(Student student) {
+    int type;
+    System.out.println();
+    System.out.println(SINGLE_DASH_LINE);
+    System.out.println("Enter #" + student.getSeat() + " " + student.getName() + " Attendance");
+    System.out.println(SINGLE_DASH_LINE);
+    System.out.println("0 = Absent");
+    System.out.println("1 = Present");
+    System.out.println(SINGLE_DASH_LINE);
+    type = Input.getIntRange("Enter Attendance Type: ", 0, 1);
+    System.out.println(SINGLE_DASH_LINE);
+    try {
+        student.updateAttendance(type);
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println("Unable to update student's attendance!");
+    }
+    student.displayAttendance();
 }
+
+/**
+ * Display the updated attendance report for both courses. Calls the Course's
+ * displayCourseAttendance for both section1 and section2.
+ */
+private void updateAttendanceReport() {
+    section1.displayCourseAttendance();
+    section2.displayCourseAttendance();
 }
+
+/**
+ * Display all students' detailed attendance. Calls the displayDetailedAttendance
+ * for both section1 and section2.
+ */
+private void displayAttendanceStatus() {
+    section1.displayDetailedAttendance();
+    section2.displayDetailedAttendance();
+} // end of displayAttendanceStatus
+
+/**
+ * Main method that creates the AttendanceApp object and then
+ * sets up the courses and takes attendance via menu options.
+ * <br>
+ * 1) creates a new AttendanceApp<br>
+ * 2) calls the displayAppHeading method<br>
+ * 3) using a try-catch calls setupCourses and mainMenu methods<br>
+ * <br>
+ * @param args No command line input args are used for this application
+ */
+public static void main(String[] args) {
+    AttendanceApp app = new AttendanceApp();
+    app.displayAppHeading();
+    try {
+        app.setupCourses();
+        app.mainMenu();
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println("Sorry but this program ended with an error. Please contact support!");
+    }
+    Input.sc.close();
+} // end of main
+} // end of AttendanceApp
+
 //main menu , detail report course attendence, student attendence
